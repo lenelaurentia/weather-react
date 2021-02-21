b3ff07aac8381081aa7533651e98de184485b1f9
lenelaurentia <lenelaurentia@gmail.com>
2021-02-20 17:24:13 -0800

    Fixed api (https) in Weather

--- src/Weather.js
+++ src/Weather.js
@@ -25,7 +25,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "4630bdec4a9dba06ebc0c195b85646bc";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
